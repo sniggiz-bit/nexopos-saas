b8835de860672318b29601f8ca9141245ef98be7
Niggiz Dev <sniggiz@gmail.com>
2026-03-04 23:41:10 -0300

    chore: mover config de seed a prisma.config

--- apps/api/prisma.config.js
+++ apps/api/prisma.config.js
@@ -5,6 +5,7 @@ module.exports = {
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: 'npx tsx prisma/seed.ts',
   },
   datasource: {
     url: process.env.DATABASE_URL || "postgresql://postgres:postgres@postgres:5432/nexopos_db",
