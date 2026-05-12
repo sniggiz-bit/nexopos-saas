966fb84915371d4dd9e8d16d00a3a05f7e7591a0
Niggiz Dev <sniggiz@gmail.com>
2026-05-12 19:53:20 -0400

    fix: guardar e inyectar providers completos en SalesModule

--- apps/api/src/sales/sales.module.ts
+++ apps/api/src/sales/sales.module.ts
@@ -2,14 +2,20 @@ import { Module } from '@nestjs/common';
 import { SalesService } from './sales.service';
 import { SalesController } from './sales.controller';
 import { EventsModule } from '../events/events.module';
-import { InternalReceiptService } from '../dte/internal-receipt.service'; // <--- LA RUTA CORREGIDA
+import { InternalReceiptService } from '../dte/internal-receipt.service';
+import { CreditsService } from '../credits/credits.service';
+import { DteService } from '../dte/dte.service'; // <--- AGREGADO
+import { InventoryService } from '../inventory/inventory.service'; // <--- AGREGADO
 
 @Module({
   imports: [EventsModule],
   controllers: [SalesController],
   providers: [
     SalesService,
-    InternalReceiptService
+    InternalReceiptService,
+    CreditsService,
+    DteService, // <--- INYECTADO CON COMA
+    InventoryService // <--- INYECTADO
   ],
 })
 export class SalesModule { }
