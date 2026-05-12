e5b6c8dae009c76fcf2a329f807120d0be564c8a
Niggiz Dev <sniggiz@gmail.com>
2026-05-12 18:49:34 -0400

    fix: usar ruta correcta de dte para InternalReceiptService

--- apps/api/src/sales/sales.module.ts
+++ apps/api/src/sales/sales.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { SalesService } from './sales.service';
 import { SalesController } from './sales.controller';
 import { EventsModule } from '../events/events.module';
-import { InternalReceiptService } from '../receipts/internal-receipt.service'; // <--- Importamos el servicio rebelde
+import { InternalReceiptService } from '../dte/internal-receipt.service'; // <--- LA RUTA CORREGIDA
 
 @Module({
   imports: [EventsModule],
   controllers: [SalesController],
   providers: [
     SalesService,
-    InternalReceiptService // <--- ¡AQUÍ ESTÁ LA PIEZA QUE FALTABA!
+    InternalReceiptService
   ],
 })
 export class SalesModule { }
