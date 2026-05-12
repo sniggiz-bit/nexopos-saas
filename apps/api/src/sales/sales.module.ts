fe35798c7d01bc83da1aec3eadc888fe76fc2218
Niggiz Dev <sniggiz@gmail.com>
2026-05-12 18:40:03 -0400

    fix: inyectar InternalReceiptService en SalesModule

--- apps/api/src/sales/sales.module.ts
+++ apps/api/src/sales/sales.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SalesService } from './sales.service';
 import { SalesController } from './sales.controller';
-import { EventsModule } from '../events/events.module'; // <-- Asegura la ruta relativa
+import { EventsModule } from '../events/events.module';
+import { InternalReceiptService } from '../receipts/internal-receipt.service'; // <--- Importamos el servicio rebelde
 
 @Module({
-  imports: [EventsModule], // <-- AGREGA EL MODULE AQUÍ
+  imports: [EventsModule],
   controllers: [SalesController],
-  providers: [SalesService],
+  providers: [
+    SalesService,
+    InternalReceiptService // <--- ¡AQUÍ ESTÁ LA PIEZA QUE FALTABA!
+  ],
 })
 export class SalesModule { }
