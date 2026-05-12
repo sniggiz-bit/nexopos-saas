b4f21ec60d56f9d3641bc5715322637a6fbbf969
Niggiz Dev <sniggiz@gmail.com>
2026-05-12 02:08:38 -0400

    arch: volver EventsModule global para cerrar dependencias

--- apps/api/src/events/events.module.ts
+++ apps/api/src/events/events.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
 import { EventsService } from './events.service';
 
+@Global() // <--- ¡ESTA ES LA MAGIA! Lo hace visible en todo NexoPOS sin importar el módulo
 @Module({
     providers: [EventsGateway, EventsService],
-    exports: [EventsGateway, EventsService], // <-- EXPORTAR AMBOS AQUÍ
+    exports: [EventsGateway, EventsService],
 })
 export class EventsModule { }
