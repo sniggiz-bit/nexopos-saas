7b5e20c185c3568ebf907b304c207a6e737449d2
Niggiz Dev <sniggiz@gmail.com>
2026-05-13 16:10:22 -0400

    fix: importar EventsModule en PurchasesModule

--- apps/api/src/purchases/purchases.module.ts
+++ apps/api/src/purchases/purchases.module.ts
@@ -3,9 +3,10 @@ import { PurchasesService } from './purchases.service';
 import { PurchasesController } from './purchases.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
+import { EventsModule } from '../events/events.module';
 
 @Module({
-    imports: [PrismaModule, AuthModule],
+    imports: [PrismaModule, AuthModule, EventsModule],
     controllers: [PurchasesController],
     providers: [PurchasesService],
     exports: [PurchasesService],
