830a7bf9f017e3a070521f9f6eddd1b4cc4e6266
Niggiz Dev <sniggiz@gmail.com>
2026-08-11 09:52:58 -0400

    fix: correctly restore join in main.ts and update prisma client import

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, Catch, ExceptionFilter, ArgumentsHost, HttpException } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 @Catch()
